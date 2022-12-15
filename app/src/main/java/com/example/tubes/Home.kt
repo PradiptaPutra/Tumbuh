23d4062e0caffa2fd61d642a4ae1cdbadad0d525
Pradipta Putra <20523004@students.uii.ac.id>
2022-12-15 14:43:49 +0700

    https://github.com/PradiptaPutra/Tumbuh.git

--- app/src/main/java/com/example/tubes/Home.kt
+++ app/src/main/java/com/example/tubes/Home.kt
@@ -1,14 +1,19 @@
 package com.example.tubes
 
+import android.content.ClipData
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 import androidx.fragment.app.Fragment
 import com.example.tubes.databinding.ActivityHomeBinding
-import com.example.tubes.databinding.ActivityMainBinding
+
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.text.DateFormat;
 import java.util.Calendar;
 
@@ -23,9 +28,42 @@ class Home : AppCompatActivity() {
         val currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.time)
         val textViewDate = findViewById<TextView>(R.id.text_view_date)
         textViewDate.text = currentDate
-    }
-}
 
+        val buttonClick = findViewById<FloatingActionButton>(R.id.fab)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+        val itemClickListener = findViewById<BottomNavigationItemView>(R.id.notes)
+        itemClickListener.setOnClickListener{
+            val intent = Intent(this,Notes::class.java)
+            startActivity(intent)
+
+        }
+
+        val itemClickListener2 = findViewById<BottomNavigationItemView>(R.id.pomodoro)
+        itemClickListener2.setOnClickListener{
+            val intent = Intent(this,Pomodoro::class.java)
+            startActivity(intent)
+
+        }
+        val itemClickListener3 = findViewById<BottomNavigationItemView>(R.id.quiz)
+        itemClickListener3.setOnClickListener{
+            val intent = Intent(this,activity_start::class.java)
+            startActivity(intent)
+
+        }
+        val itemClickListener4 = findViewById<BottomNavigationItemView>(R.id.reminder)
+        itemClickListener4.setOnClickListener{
+            val intent = Intent(this,Reminder::class.java)
+            startActivity(intent)
+
+
+        }
+
+    }
+
+}
 
 
 
